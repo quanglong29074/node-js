7d600f0cf39c364a31ee113215dd8738a50c3b02
quanglong29074 <114908074+quanglong29074@users.noreply.github.com>
2023-12-27 15:04:55 +0700

    register

--- ch06/11-more-robust-form-processing.js
+++ ch06/11-more-robust-form-processing.js
@@ -1,29 +1,36 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
 const expressHandlebars = require('express-handlebars').engine;
-const app = express();
+const bodyParser = require('body-parser')
+const app = express()
 
-app.engine('handlebars', expressHandlebars({
-    defaultLayout: 'main'
-}));
-app.set('view engine', 'handlebars');
+
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.get('*', (req, res) => res.render('11-home'));
+app.get('/thank-you', (req, res) => res.render('11-thank-you'))
+app.get('/contact-error', (req, res) => res.render('11-contact-error'))
+
+
+app.get('*', (req, res) => res.render('11-home'))
+
 app.post('/process-contact', (req, res) => {
     try {
+
+
         if (req.body.simulateError) throw new Error("error saving contact!")
-        console.log(`contact from ${res.body.name} <${req.body.email}>`)
+        console.log(`contact from ${req.body.name} <${req.body.email}>`)
         res.format({
-            'text/plain': () => res.redirect(303, '/thank-you'),
+            'text/html': () => res.redirect(303, '/thank-you'),
             'application/json': () => res.json({ success: true }),
         })
     } catch (err) {
+
         console.error(`error processing contact from ${req.body.name}` +
-            `<${req.body.email}>`
-        )
+            `<${req.body.email}>`)
         res.format({
             'text/html': () => res.redirect(303, '/contact-error'),
             'application/json': () => res.status(500).json({
@@ -31,7 +38,8 @@ app.post('/process-contact', (req, res) => {
             }),
         })
     }
+
 })
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`\nnavigate to http://localhost:${port}\n`));
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`\nnavigate to http://localhost:${port}\n`))
